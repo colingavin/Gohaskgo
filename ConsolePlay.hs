414b866aea1ac6ed1ff3a402adb4dd479d9f5a22
Colin Gavin <colin@willowtree-dev.net>
2012-11-25 13:37:35 -0800

    Modify ConsolePlay to play with UCT.

--- ConsolePlay.hs
+++ ConsolePlay.hs
@@ -1,11 +1,14 @@
 module Main where
 
+import Data.Random
+import Data.Random.Source.DevRandom
 import Control.Monad
 import Text.ParserCombinators.Parsec
 import Data.Set (Set)
 import qualified Data.Set as Set
 
 import GoModel
+import UCT
 
 
 -- User input parsing (using Parsec)
@@ -28,32 +31,39 @@ commaSeparatedPoint = actualPair <|> onlyReturn
 
 -- User interaction
 
+type MoveSource = IncompleteGame -> IO (Maybe Point)
+
 -- Trys to get and parse a point, returns Nothing if the user passes
-getPoint :: IO (Maybe Point)
-getPoint = do
+getPoint :: MoveSource
+getPoint gm = do
     inPt <- getLine
     case parse commaSeparatedPoint "" (inPt ++ "\n") of
         Right result -> do
             return result
         Left _ -> do
             putStrLn "Invalid input. Try again."
-            getPoint
+            getPoint gm
+
+getUCTMove :: MoveSource
+getUCTMove gm = do
+    move <- sampleFrom DevURandom $ uctRespond gm 200 1
+    return $ Just move
 
 printMostRecentPosition :: Game a => a -> IO ()
 printMostRecentPosition = putStrLn . prettyPrintPosition . latestPosition
 
-playMove :: Maybe Point -> IncompleteGame -> IO ()
-playMove (Just pt) gm = case play gm pt of
-    Right ng -> interactGame $ Right ng
+playMove :: Maybe Point -> IncompleteGame -> [MoveSource] -> IO ()
+playMove (Just pt) gm players = case play gm pt of
+    Right ng -> interactGame (Right ng) (drop 1 players)
     Left err -> do
         putStrLn $ "Invalid move: " ++ (show err)
-        interactGame $ Right gm
-playMove Nothing gm = interactGame $ pass gm
+        interactGame (Right gm) players
+playMove Nothing gm players = interactGame (pass gm) (drop 1 players)
 
 -- Drives the main loop of interaction
-interactGame :: AnyGame -> IO ()
+interactGame :: AnyGame -> [MoveSource] -> IO ()
 -- Is the game over?
-interactGame (Left gm) = do 
+interactGame (Left gm) _ = do 
     (printMostRecentPosition gm)
     putStrLn "Game over."
     let (blackScore, whiteScore) = score gm
@@ -62,11 +72,13 @@ interactGame (Left gm) = do
     putStrLn $ (show $ winner gm) ++ " won."
 
 -- If the game isn't over, try to get and play a point from the current player
-interactGame (Right gm) = do
+interactGame (Right gm) players = do
     printMostRecentPosition gm
-    print $ latestPosition gm
+    -- print $ latestPosition gm
     putStrLn $ (show (getToPlay gm)) ++ " to play. (Return to pass.)"
-    move <- getPoint
-    playMove move gm
+    move <- (head players) gm
+    playMove move gm players
 
-main = interactGame $ Right $ makeNewGame 9
+players = cycle [getPoint, getUCTMove]
+
+main = interactGame (Right $ makeNewGame 9) players
