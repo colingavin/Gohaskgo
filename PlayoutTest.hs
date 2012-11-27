cf602bf907190655755fd65c6ad5cc468c47a94f
Colin Gavin <colin@willowtree-dev.net>
2012-11-26 17:32:46 -0800

    Allow playout tests starting from specific positions.

--- PlayoutTest.hs
+++ PlayoutTest.hs
@@ -1,14 +1,30 @@
 module Main where
 
+import System.Environment
 import Data.Random
 import Data.Random.Source.DevRandom
 
 import Playout
 import GoModel
+import AsciiBoardParser
+
+readProblem :: String -> IO (Maybe IncompleteGame)
+readProblem filename = do
+    problemText <- readFile filename
+    case parseToPosition problemText of
+        Nothing -> return $ Nothing
+        Just pos -> do
+            return $ Just $ makeGameFromPosition pos White
 
 main = do
-    let emptyGame = Right $ makeNewGame 9
-    let afterRand = randomPlayout emptyGame
-    let randWin = randomWinner emptyGame
+    args <- getArgs
+    game <- if length args > 0
+        then do
+            problem <- readProblem (args !! 0)
+            return $ case problem of
+                Nothing -> error "Invalid filename."
+                Just gm -> Right gm
+        else return $ Right $ makeNewGame 9
+    let afterRand = randomPlayout game
     a <- sampleFrom DevURandom afterRand
     putStrLn $ prettyPrintPosition $ latestPosition a
